097c434ec00bae13f7a5338fa81f1857afabf24a
mukilan <mukilan@5gindia.net>
2017-05-12 16:58:28 +0530

    Added dependencies tomcat, camel-core, camel-ftp.

--- collateral-substitution/src/main/java/com/cos/MyRoute.java
+++ collateral-substitution/src/main/java/com/cos/MyRoute.java
@@ -8,6 +8,6 @@ public class MyRoute extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
-		from("sftp://linuxuser@192.168.1.47/home/linuxuser/Documents?password=p@ssw0rd&passiveMode=true&noop=true").to("file:///home/linuxuser/Documents/camel/2");
+		from("sftp://linuxuser@localhost/Documents/camel/1?password=mukilan123&passiveMode=true").to("file:///home/linuxuser/Documents/camel/2");
 	}
 }
